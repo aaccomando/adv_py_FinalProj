b7a62dec344ddb388269c61e8b381e656669f662
Malavkumar Patel <mpatel26@mail.usf.edu>
2018-04-07 18:43:26 -0400

    added db.sql

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE foodDB;
+
+CREATE TABLE users(
+    uid serial PRIMARY KEY,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    pwdhash VARCHAR(100) NOT NULL,
+);
+
+INSERT INTO users (firstname, lastname, email, pwdhash) VALUES (
+    'Malav', 'Patel', 'malavpatel@gmail.com', 'malavpatel'
+);
