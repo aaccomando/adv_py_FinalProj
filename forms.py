0f1b99a328145aee729eb57b3ad5a2e2b3b09876
Malavkumar Patel <mpatel26@mail.usf.edu>
2018-04-15 17:53:14 -0400

    updated and added ingredient form

--- forms.py
+++ forms.py
@@ -13,3 +13,7 @@ class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
     password = PasswordField('Password', validators=[DataRequired("Please enter a password.")])
     submit = SubmitField("Sign in")
+
+class IngredientForm(Form):
+    ingredient = StringField('Ingredient', validators=[DataRequired("Please enter an ingredients.")])
+    submit = SubmitField("Search")
